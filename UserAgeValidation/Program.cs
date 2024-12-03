a7ac7a77d94ec5a57e12b8a1d1c51d1c2d1ee2cd
Carl <carlrobert.kalev@gmail.com>
2024-12-03 10:57:53 +0200

    create new app

---
+++ UserAgeValidation/Program.cs
@@ -0,0 +1,23 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+//rakendus küsib sugu (m/f)
+//rakendus küsib perekonna nime
+//lähtudeskasutaja kasutaja valikust, rakendus tervitab kasutajat järgmiselt
+// welcome (MR. keegi keegi)
+
+Console.WriteLine("Please, select your gender (M/F):");
+
+char userGender = Char.Parse(Console.ReadLine());
+
+if (userGender == 'M')
+{
+    Console.WriteLine("Welcome, Mr.!");
+}
+else if (userGender == 'F')
+{
+    Console.WriteLine("Welcome, Ms.!");
+}
+else
+{
+    Console.WriteLine("Welcome!");
+}
